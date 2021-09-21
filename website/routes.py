44ffbd647ff858697e0c94dcbc88be1fb1695827
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2021-09-21 10:37:54 +0300

    lab_4

--- website/routes.py
+++ website/routes.py
@@ -1,10 +1,9 @@
 from website.models import Users, Posts, PostForm
 from website import create_app
-
 from flask import render_template, request, session, redirect, url_for, flash
 from passlib.hash import sha256_crypt
 from website import db
-from flask_login import login_user, current_user
+from flask_login import login_user
 from flask_toastr import Toastr
 import logging
 
@@ -16,12 +15,12 @@ app = create_app("config.MyConfig")
 toastr = Toastr(app)
 
 @app.route('/')
-async def index():
+def index():
     page = request.args.get('page', 1, type=int)
     all_posts = db.session.query(Posts.post_time, Posts.title, Posts.content, Users.username).join(Posts)\
         .order_by(Posts.post_time.desc()).paginate(page=page, per_page=3)
     session.permanent = True
-    
+
     if "username" not in session:
         logging.info('Unknow user connected')
         return render_template('index.html', status="disabled", posts=all_posts)
@@ -30,26 +29,26 @@ async def index():
         logging.info('User {} connected'.format(username))
         flash("You are already logged in as %s" % username)
         return render_template('index.html', posts=all_posts)
-    
+
 
 @app.route('/search', methods=['GET', 'POST'])
-async def search():
+def search():
     if request.method == "POST":
         username = request.form.get("username")
-        if await db.session.query(Users.id).filter_by(username = username).scalar():
-            user_id = await db.session.query(Users.id).filter_by(username = username).scalar()
-            user_posts = await db.session.query(Posts).filter_by(parent_id=user_id)\
+        if db.session.query(Users.id).filter_by(username = username).scalar():
+            user_id = db.session.query(Users.id).filter_by(username = username).scalar()
+            user_posts = db.session.query(Posts).filter_by(user_id=user_id)\
                 .order_by(Posts.post_time.desc())
             return render_template('search.html', posts=user_posts, usr=username)
         else:
             logging.info('Post not found by user - {}'.format(username))
             flash("There is no post by  %s" % username)
             return redirect(url_for('index'))
-    
-  
-    
+
+
+
 @app.route('/register', methods= ["POST", "GET"])
-async def register():
+def register():
     if "username" in session:
             username = session["username"]
             flash("You are already logged in as %s" % username)
@@ -64,12 +63,12 @@ async def register():
             if email == '' or username == '' or password == '':
                 flash("Please enter all input fields!")
                 return redirect(url_for("register", status="disabled"))
-            if await db.session.query(Users.id).filter_by(email = email).scalar() or await db.session.query(Users.id).filter_by(username = username).scalar() is not None:
+            if db.session.query(Users.id).filter_by(email = email).scalar() or db.session.query(Users.id).filter_by(username = username).scalar() is not None:
                 flash("The email or username already is being used please choose a different one or login if your an existing user")
                 return redirect(url_for("register", status="disabled"))
             register_user = Users(email = email, username = username, password = hashed)
-            await db.session.add(register_user)
-            await db.session.commit()
+            db.session.add(register_user)
+            db.session.commit()
             session["username"] = username
             flash("You have logged in as %s" % username)
             logging.info('Register user - {}'.format(username))
@@ -78,7 +77,7 @@ async def register():
 
 
 @app.route('/login', methods= ["POST", "GET"])
-async def login():
+def login():
     if "username" in session:
         username = session["username"]
         flash("You are already logged in as %s" % username)
@@ -92,11 +91,12 @@ async def login():
             if username == '' or password == '':
                 flash("Please enter all input fields!")
                 return redirect(url_for("login", status="disabled"))
-            if await db.session.query(Users.id).filter_by(username = username).scalar():
-                if await db.session.query(Users.id).filter_by(password = hashed):
-                    await db.session.query(Users.id)
+            if db.session.query(Users.id).filter_by(username = username).first():
+                if db.session.query(Users.id).filter_by(password = hashed):
+                    db.session.query(Users.id)
                     session["username"] = username
-                    login_user(Users.query.filter_by(username=username).first())
+                    db_user = Users.query.filter_by(username=username).first()
+                    login_user(db_user)
                     flash("You have logged in as %s" % username)
                     return redirect(url_for('members'))
                 flash("Password is incorect please try again")
@@ -104,45 +104,26 @@ async def login():
             flash("Username or password is incorect please try again or register!")
             logging.info('Login user - {}'.format(username))
             return redirect(url_for("login", status="disabled"))
-        
+
         return render_template('login.html', status="disabled")
 
-@app.route('/my-profile', methods= ["POST", "GET"])
-async def my_profile():
-    user = Users.query.get(current_user.id)
-
-    if request.method == "POST":
-        return render_template('my_profile.html', user=user)
-    else:
-        username = request.form.get("username")
-        if Users.query.filter_by(username=username).first():
-            return render_template('my_profile.html', message='Username already exists', user=user)
-        
-        if not username is None and len(username) > 2 and len(username) < 16:
-            user.username = username
-            await db.session.commit()
-            return redirect(url_for('my_profile'))
-        return render_template('my_profile.html', message='Invalid username', user=user)
-
-
-
 @app.route('/user/', methods= ["POST", "GET"])
-async def members():
+def members():
     if "username" in session:
         username = session["username"]
         page = request.args.get('page', 1, type=int)
-        
-        user_id = await db.session.query(Users.id).filter_by(username = username).scalar()
-        user_posts = await db.session.query(Posts).filter_by(parent_id=user_id)\
+
+        user_id = db.session.query(Users.id).filter_by(username = username).scalar()
+        user_posts = db.session.query(Posts).filter_by(user_id=user_id)\
             .order_by(Posts.post_time.desc()).paginate(page=page, per_page=3)
         form = PostForm()
         title = form['title'].data
         content = form['content'].data
 
         if request.method == "POST":
-            post = Posts(post_time=form.post_time ,title=title, content=content, parent_id=user_id)
-            await db.session.add(post)
-            await db.session.commit()
+            post = Posts(post_time=form.post_time ,title=title, content=content, user_id=user_id)
+            db.session.add(post)
+            db.session.commit()
             flash("Your post was sucesfully submited")
             return redirect(url_for('members', form=form, posts=user_posts))
         return render_template('members.html', form=form, posts=user_posts)
@@ -151,14 +132,14 @@ async def members():
 
 
 @app.route('/edit_post/<string:id>', methods= ["POST", "GET"])
-async def edit_post(id):
-    post = await db.session.query(Posts).filter_by(id = id).first()
-    post_id = await db.session.query(Posts.id).filter_by(id = id).first()
+def edit_post(id):
+    post = db.session.query(Posts).filter_by(id = id).first()
+    post_id = db.session.query(Posts.id).filter_by(id = id).first()
     form = PostForm(obj=post)
     if request.method == "POST":
         form.populate_obj(post)
-        await db.session.add(post)
-        await db.session.commit()
+        db.session.add(post)
+        db.session.commit()
         flash("The post has been updatet")
         logging.info('The post - {} - has been updatet'.format(post))
         return redirect(url_for('members'))
@@ -166,10 +147,10 @@ async def edit_post(id):
 
 
 @app.route('/delete_post/<string:id>', methods=['POST'])
-async def delete_post(id):
+def delete_post(id):
     if "username" in session:
-        await db.session.query(Posts).filter_by(id = id).delete()
-        await db.session.commit()
+        db.session.query(Posts).filter_by(id = id).delete()
+        db.session.commit()
 
         flash("Post deleted")
         logging.info('Delete post')
@@ -188,7 +169,7 @@ def logout():
         logging.info('User - {} - successfully logged out'.format("username"))
         resp = app.make_response(render_template('login.html', status="disabled"))
         resp.set_cookie('token', expires=0)
-        return resp 
+        return resp
     else:
         flash("You have been already logged out")
         return redirect(url_for("login.html"))
