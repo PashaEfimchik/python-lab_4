daf5e060e690daa4504a1e9953918fc8db886b1e
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2021-09-21 10:36:15 +0300

    lab_4

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,16 @@
 from re import I
 from flask import Flask
-from flask_login import LoginManager, login_manager, current_user
-from flask_socketio import SocketIO
+from flask_login import current_user, LoginManager, login_manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
-import socketio
 
 db = SQLAlchemy()
-socketio = SocketIO()
 
 from website.models import *
 
+login_manager = LoginManager()
+login_manager.login_view = "users.login"
 admin = Admin()
 
 class AdminMenu(AdminIndexView):
@@ -27,25 +26,21 @@ class UsersView(ModelView):
 class PostsView(ModelView):
     column_list = ['id', 'post_time', 'title', 'content']
 
-class HomeAdminView(AdminIndexView):
-    def is_accessible(self):
-        return not current_user.is_anonymous and current_user.is_admin
-    
-    def inaccessible_callback(self, name, **kwargs):
-        return super().inaccessible_callback(name, **kwargs)
-
 def create_app(config="config.MyConfig"):
     app = Flask(__name__)
     app.config.from_object(config)
-
     init_user(app)
-    
+
     admin.init_app(app, url='/', index_view=AdminMenu(name='Home'))
     admin.add_view(UsersView(Users, db.session))
     admin.add_view(PostsView(Posts, db.session))
 
-    socketio.init_app(app, cors_allowed_origins='*')
     return app
 
 def init_user(app):
     db.init_app(app)
+
+    login_manager.init_app(app)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
